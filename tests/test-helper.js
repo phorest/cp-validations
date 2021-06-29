3c4093866a8649e5631df9c7aab7f587ba15ee0e
“Drew <andrew.jr.neil@gmail.com>
2021-06-29 14:11:01 +0100

    Replace ember-cli-qunit with ember-qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,5 @@
-import resolver from './helpers/resolver';
-import {
-  setResolver
-} from 'ember-qunit';
-import { start } from 'ember-cli-qunit';
+import resolver from "./helpers/resolver";
+import { setResolver, start } from "ember-qunit";
 
 setResolver(resolver);
 start();
